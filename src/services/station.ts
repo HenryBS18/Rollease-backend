dd6e26e63abec9f88e01b21f8459b400f7ea8624
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2024-06-25 03:01:14 +0800

    Update station service

--- src/services/station.ts
+++ src/services/station.ts
@@ -1,8 +1,22 @@
+import { StationDto, Vehicle } from "../types";
 import { StationRepo } from "../repositories";
-import { Station } from "types";
+import VehicleService from "./vehicle";
 
-const getAll = async (): Promise<Station[]> => {
-  return await StationRepo.findAll();
+const getAll = async (): Promise<StationDto[]> => {
+  const stationList = await StationRepo.findAll();
+
+  const stationDto: StationDto[] = []
+
+  for (const station of stationList) {
+    const vehicleList: Vehicle[] = await VehicleService.getAllAvailableByStationId(station.id);
+
+    stationDto.push({
+      ...station,
+      vehicleList: vehicleList
+    });
+  }
+
+  return stationDto;
 }
 
 const StationService = { getAll }
