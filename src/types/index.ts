f47b7eca2a37c13438a199e2e038c574694a5e1b
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2024-06-25 03:00:53 +0800

    Add new interfaces

--- src/types/index.ts
+++ src/types/index.ts
@@ -24,3 +24,24 @@ export interface Station {
   coordinates: Coordinates;
   imageUrl: string;
 }
+
+export interface StationDto extends Station {
+  vehicleList: Vehicle[]
+}
+
+export enum VehicleType {
+  scooter = "Scooter",
+  bike = "Bike"
+}
+
+export interface Vehicle {
+  id: number;
+  name: string;
+  type: VehicleType;
+  imageUrl: string;
+  battery: number;
+  inUse: boolean;
+  currentStationId: number;
+  argoPrice: number;
+  currentCoordinates: Coordinates;
+}
